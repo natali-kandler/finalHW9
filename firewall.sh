bf5d1ebfd7a27a6e53c2777725f8044b86ac2a97
zivchen9993 <ziv.chen@campus.technion.ac.il>
2021-01-26 23:55:55 +0200

    update the bash comments

--- firewall.sh
+++ firewall.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
+# to make sort work propperly
 export LC_ALL=C
+
+# initializing parse of the rules and packets  
 remain_rules=`cat "$1" | grep -o '^[^#]*'`
-
 remain_packets=`(tee) | grep -o '^[^#]*'`
-echo "$remain_rules" > rules_input_after.txt
 
-
-> packets_output.txt
-
+# initializing variable
 packets_to_print=""
+
+# loop over all of the rules and checking them column by column while
+# checking only the remaining packets
 while IFS=, read -r first second third fourth; do
    surviving_packets=`echo "$remain_packets" | ./firewall.exe "$first"`
    surviving_packets=`echo "$surviving_packets" | ./firewall.exe "$second"`
@@ -17,8 +20,11 @@ while IFS=, read -r first second third fourth; do
    packets_to_print+=`echo -e "$surviving_packets"`
    packets_to_print+="\n"
 done <<< "$remain_rules"
+
+# parsing the output packets
 packets_to_print=`echo -e "$packets_to_print" | sort | uniq`
-echo -e "$packets_to_print" > beS_packets_output.txt
-packets_to_print=`echo -e "$packets_to_print" | sort | uniq |tr -d "[:blank:]" | grep -o '^[^#]*'`
+packets_to_print=`echo -e "$packets_to_print" | tr -d "[:blank:]"`
+packets_to_print=`echo -e "$packets_to_print" | grep -o '^[^#]*'`
+
+# print output
 echo -e "$packets_to_print"
-echo -e "$packets_to_print" > packets_output.txt
